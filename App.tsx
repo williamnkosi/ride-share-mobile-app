e58f108f7ac0f61a65969072995f2d3f8fdf71d8
William Nkosi <william.dev.acc@gmail.com>
2023-03-19 04:21:03 -0400

    Added NavOptions components and UI components from npm

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Provider } from "react-redux";
 import HomeScreen from "./screens/HomeScreen";
 import { store } from "./store";
@@ -7,7 +8,9 @@ import { store } from "./store";
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <HomeScreen />
+      </SafeAreaProvider>
     </Provider>
   );
 }
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-
+import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
 type Props = {};
 
 const HomeScreen: any = (props: Props) => {
   return (
-    <SafeAreaView>
-      <Text>This is the HomeScreen</Text>
+    <SafeAreaView style={tw`bg-red h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+          source={{ uri: "https://links.papareact.com/gzs" }}
+        />
+        <NavOptions />
+      </View>
     </SafeAreaView>
   );
 };
