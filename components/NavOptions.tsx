9f805e2c5a1c5f7af178ab96b3dfc954e63ce0a8
William Nkosi <william.dev.acc@gmail.com>
2023-03-19 05:55:13 -0400

    Added google-places-autocomplete

--- components/NavOptions.tsx
+++ components/NavOptions.tsx
@@ -10,6 +10,7 @@ import {
 import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const data = [
   {
@@ -28,13 +29,17 @@ const data = [
 type Props = {};
 
 const NavOptions = (props: Props) => {
+  const navigation = useNavigation();
   return (
     <FlatList
       data={data}
       horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
           <View>
             <Image
               style={{ width: 120, height: 120 }}
